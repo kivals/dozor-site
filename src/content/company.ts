9d4d5bd17de24ae7ec540410431f7291e9a06b04
vvmelnik <kivals.90@yandex.ru>
2026-07-29 14:40:17 +0300

    switch email to info@el-dozor.ru

--- src/content/company.ts
+++ src/content/company.ts
@@ -3,7 +3,7 @@ export const company = {
   legalName: "Группа компаний «Дозор»",
   slogan: "Охрана объектов с персональным контролем",
   phone: "+7 800 222 88 19",
-  email: "gp_dozor@yandex.ru",
+  email: "info@el-dozor.ru",
   address: "г. Электросталь",
   geo: { lat: 55.784748, lng: 38.444741 },
   social: {
--- src/content/contacts.ts
+++ src/content/contacts.ts
@@ -2,6 +2,6 @@ export const contacts = {
   phoneNote: "круглосуточно",
   phone: "8 800 222 88 19",
   phoneHref: "tel:+78002228819",
-  email: "gp_dozor@yandex.ru",
+  email: "info@el-dozor.ru",
   address: ["м.о., г. Электросталь,", "ул. Карла Маркса, 1"],
 };

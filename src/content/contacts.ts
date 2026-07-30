dee99a3e1cb463789f66f2b114b3843e00246861
vvmelnik <kivals.90@yandex.ru>
2026-07-30 22:49:50 +0300

    capitalize М.О. in address

--- src/content/contacts.ts
+++ src/content/contacts.ts
@@ -3,5 +3,5 @@ export const contacts = {
   phone: "8 800 222 88 19",
   phoneHref: "tel:+78002228819",
   email: "info@el-dozor.ru",
-  address: ["м.о., г. Электросталь,", "ул. Карла Маркса, 1"],
+  address: ["М.О., г. Электросталь,", "ул. Карла Маркса, 1"],
 };
